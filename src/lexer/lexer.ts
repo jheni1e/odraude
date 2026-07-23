6bf4403502a8581a900b5b513cdc36646ef85bb6
Jhenie <highhalma@gmail.com>
2026-07-23 15:29:57 -0300

    FEAT: lexer now identifies strings

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -2,47 +2,65 @@ import { Token } from "./token";
 import { TokenType } from "./token-type";
 
 export class Lexer {
-    constructor(private source: string) {}
+  constructor(private source: string) {}
 
-    private current = 0;
-    private start = 0;
-    private line = 1;
-    private tokens: Token[] = [];
+  private current = 0;
+  private start = 0;
+  private line = 1;
+  private tokens: Token[] = [];
 
-    private advance(): string {
-        return this.source[this.current++];
+  private advance(): string {
+    return this.source[this.current++];
+  }
+
+  scanTokens() {
+    while (this.current < this.source.length) {
+      this.start = this.current;
+
+      const char = this.advance();
+
+      switch (char) {
+        case "\n":
+          break;
+        case "=":
+          this.addToken(TokenType.EQUAL);
+          break;
+        case "+":
+          this.addToken(TokenType.PLUS);
+          break;
+        case "-":
+          this.addToken(TokenType.MINUS);
+          break;
+        case ">":
+          this.addToken(TokenType.GREATER);
+          break;
+        case "<":
+          this.addToken(TokenType.LESS);
+        case ";":
+          this.addToken(TokenType.SEMICOLON);
+          break;
+        case '"':
+          let string = "";
+          let c = this.advance();
+
+          while (c != '"') {
+            string = string.concat(c);
+            c = this.advance();
+          }
+
+          this.addToken(TokenType.STRING, string);
+          break;
+      }
     }
 
-    scanTokens() {
-        while (this.current < this.source.length) {
-            this.start = this.current;
+    this.addToken(TokenType.EOF);
 
-            const char = this.advance();
+    return this.tokens;
+  }
 
-            switch(char) {
-                case ' ':
-                    break;
-                case '=':
-                    this.addToken(TokenType.EQUAL)
-                    break;
-            }
-        }
+  private addToken(type: TokenType, literal: unknown = null) {
+    const lexeme = this.source.substring(this.start, this.current);
 
-        this.addToken(TokenType.EOF);
-
-        return this.tokens;
-    }
-
-    private addToken(type: TokenType, literal: unknown = null) {
-        const lexeme = this.source.substring(this.start, this.current);
-
-        this.tokens.push(
-            new Token(
-                type,
-                lexeme,
-                literal,
-                this.line
-            )
-        );
-    }
+    this.tokens.push(new Token(type, lexeme, literal, this.line));
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Lexer } from "../src/lexer/lexer";
 const code = `
 let name = "Jhenie"
 let idade = 19
+show(32 + 35)
 `;
 
 const lexer = new Lexer(code);
