a3f4fdc08a16c5739e662c3358083bf5d0da5f28
Jhenie <highhalma@gmail.com>
2026-07-27 14:02:36 -0300

    FIX: adapted main.ts to use parser

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { Lexer } from "../src/lexer/lexer";
+import { Parser } from "./parser/parser";
 
 const code = `
 let name = "Jhenie";
 let idade = 19;
-show(32 + 35);
+show name;
+show idade;
 -- oiiiii
 `;
 
 const lexer = new Lexer(code);
+const tokens = lexer.scanTokens();
 
-console.log(lexer.scanTokens());
+console.log(tokens);
+
+const parser = new Parser(tokens);
+const ast = parser.parse();
+
+console.log(ast);
