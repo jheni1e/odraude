56f25c318396f8ffa5034774332334f80c4dfd7f
Jhenie <highhalma@gmail.com>
2026-08-04 10:18:38 -0300

    FEAT: new methods on the symbol table

--- src/semantic/symbol-table.ts
+++ src/semantic/symbol-table.ts
@@ -5,10 +5,6 @@ export class SymbolTable {
   private symbols = new Map<string, SymbolInfo>();
 
   define(name: string, symbol: SymbolInfo): void {
-    if (this.symbols.has(name)) {
-      throw new Error(`Symbol '${name}' already defined.`);
-    }
-
     this.symbols.set(name, symbol);
   }
 
@@ -17,6 +13,10 @@ export class SymbolTable {
   }
 
   has(name: string): boolean {
+    return this.lookup(name) !== undefined;
+  }
+
+  hasCurrent(name: string): boolean {
     return this.symbols.has(name);
   }
 
